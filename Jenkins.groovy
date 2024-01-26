cc9d52fdeb3a8e2100f08ea0e43a15fca18b946f
NAGASIRISHA17 <ponnagantinagasirisha@gmail.com>
2024-01-26 12:32:37 -0600

    Add Jenkins file

---
+++ Jenkins.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+    stages{
+        stage('Checkout'){
+
+        }
+        stage('Build'){
+
+        }
+        stage('Test'){
+
+        }
+        stage('Scan'){
+
+        }
+        stage('Quality'){
+
+        }
+        stage('Artifactory'){
+
+        }
+        stage('dev'){
+
+        }
+        stage('stage'){
+
+        }
+        stage('production'){
+            
+        }
+    }
+}
